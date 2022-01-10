d844129bd62f5f8e6fcc20529a4348a71dcdffd3
rishabkashyap <rishabkashyap16@gmail.com>
2022-01-11 00:13:28 +0530

    swagger and configuration

--- course-management-service/src/main/java/com/course/management/service/CourseManagementServiceApplication.java
+++ course-management-service/src/main/java/com/course/management/service/CourseManagementServiceApplication.java
@@ -2,12 +2,40 @@ package com.course.management.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CourseManagementServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseManagementServiceApplication.class, args);
+	}
+
+	@Bean
+	public Docket swaggerConfiguration(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.course.management.service"))
+				.build()
+				.apiInfo(apiDetails());
+	}
+	private ApiInfo apiDetails() {
+		return new ApiInfo("Student Course Management API",
+				"API for Student-course-Management",
+				"1.0",
+				"Free to use",
+				new springfox.documentation.service.Contact("Rishab Ranjan", "null", "rishabkashyap16@gmail.com"),
+				"API LICENSE",
+				"http://localhost:8768",
+				Collections.emptyList());
 	}
 
 }
